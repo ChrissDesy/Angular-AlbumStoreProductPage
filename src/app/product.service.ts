007ff5828491958d143f17d8312135b55c7535ac
Chriss Desmonds <chimbadzwa.christopherdesmond@gmail.com>
2021-11-02 23:40:11 +0200

    task: module 7 complete

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { map } from 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 import { Album } from './album';
 import { Product } from './product';
 
@@ -26,8 +26,10 @@ export class ProductService {
 
   getProducts(): Observable<Product[]>{
     return this._http.get(this._productsUrl)
-    .map(
-      response => <Product[]>response.json()
+    .pipe(
+      map(
+        response => <Product[]>response.json()
+      )
     )
   }
 
